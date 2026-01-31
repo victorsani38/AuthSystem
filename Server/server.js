4dc9889622fb92b8019a2da4c4e28c53f04208a0
Victor Sani <sanivictor38@gmail.com>
2026-01-31 11:49:38 +0100

    fixed es7 issue

--- Server/server.js
+++ Server/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   // Catch-all for SPA routes
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
