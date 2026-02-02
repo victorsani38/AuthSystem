0c073bf8e68c8211b5f7cf76f013b7e0a83422e2
victorsani38 <sanivictor38@gmail.com>
2026-02-02 14:45:20 +0100

    Replace window.location with navigate for login

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -24,8 +24,7 @@ const Login = () => {
         if(res.data.success){
         setUser(res.data.user)
         toast.success("login successfully")
-        window.location.href = "/dashboard";
-        //navigate("/dashboard",{replace:true});
+        navigate("/dashboard",{replace:true});
         setLoading(false) 
         setData({email:"", password:""})
         }
