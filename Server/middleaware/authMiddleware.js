584a4a9da060dfec9de5bc430bf2a0c120e38499
victorsani38 <sanivictor38@gmail.com>
2026-02-02 17:21:28 +0100

    Use optional chaining for token retrieval

--- Server/middleaware/authMiddleware.js
+++ Server/middleaware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 export const protect = async(req, res, next) => {
-    const token = req.cookies.token
+    const token = req.cookies?.token
     if(!token){
     return res.status(401).json({success:false, message:"user not authorized"});
     }
