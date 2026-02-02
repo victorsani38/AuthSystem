152652a8a6a22103cea1f428c803eaf327122163
victorsani38 <sanivictor38@gmail.com>
2026-02-02 14:35:51 +0100

    Add COOKIE_DOMAIN to auth token cookie options

--- Server/utils/generateAuthToken.js
+++ Server/utils/generateAuthToken.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken"
 export const generateAuthToken = (res, id) => {
     const token = jwt.sign({id},process.env.JWT_SECRETE, {expiresIn:"1d"})
+    const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || undefined;
     res.cookie("token", token, {
         httpOnly:true, 
         secure:true,
         sameSite:"none",
-        maxAge:24*60*60*1000
+        maxAge:24*60*60*1000,
+        domain:COOKIE_DOMAIN
     })
     return token
 }
