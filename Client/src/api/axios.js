4291959002115468756f8778b40625364ec42c8c
victorsani38 <sanivictor38@gmail.com>
2026-02-02 16:48:51 +0100

    Add headers to axios instance
    Added headers to axios instance for JSON content type.

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URI
 const API = axios.create({
     baseURL:BASE_URL + "/api",
-    withCredentials:true
+    withCredentials:true,
+    headers: {
+    "Content-Type": "application/json",
+  },
 })
 
 export default API 
