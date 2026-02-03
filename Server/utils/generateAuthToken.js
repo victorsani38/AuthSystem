a269893b47a64cbd7f11569743e6be7ab0a4c75d
victorsani38 <sanivictor38@gmail.com>
2026-02-03 10:46:55 +0100

    Remove COOKIE_DOMAIN from auth token cookie settings
    Removed COOKIE_DOMAIN from cookie options.

--- Server/utils/generateAuthToken.js
+++ Server/utils/generateAuthToken.js
@@ -1,13 +1,11 @@
 import jwt from "jsonwebtoken"
 export const generateAuthToken = (res, id) => {
     const token = jwt.sign({id},process.env.JWT_SECRETE, {expiresIn:"1d"})
-    const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || undefined;
     res.cookie("token", token, {
         httpOnly:true, 
         secure:true,
         sameSite:"none",
         maxAge:24*60*60*1000,
-        domain:COOKIE_DOMAIN
     })
     return token
 }
