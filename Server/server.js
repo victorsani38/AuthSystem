4d266f1ab8434317233bef4852a32945c9a8eafd
victorsani38 <sanivictor38@gmail.com>
2026-02-03 10:40:03 +0100

    Refactor server.js by removing unused code
    Removed commented-out code and cleaned up CORS configuration.

--- Server/server.js
+++ Server/server.js
@@ -3,23 +3,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectionDb } from "./db/connectionDb.js"; 
-import authRoute from "./route/authRoute.js" 
-// import path from "path";
-// import { fileURLToPath } from "url";
+import authRoute from "./route/authRoute.js"
 
 const app = express();
-
-
-//const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 const allowedOrigins = [
   "https://auth-client-orcin-three.vercel.app",
 ]
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
-//app.use(cors({origin:"http://localhost:5173", credentials:true})); 
-
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -30,28 +22,10 @@ app.use(
       }
     },
     credentials: true,
-  })
-  
+  })  
 )
 app.use('/api/users', authRoute)
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// if (process.env.NODE_ENV === "production") {
-//   // Serve static files from React build
-//   app.use(express.static(path.join(__dirname, "../client/dist")));
-
-//   // Catch-all for SPA routes
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-//   });
-// }
-
 const port = process.env.PORT 
-
-
-
-
 app.listen(port, ()=> {
     connectionDb()
     console.log(`Server running on http://localhost:${port}`)
